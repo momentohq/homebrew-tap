9927e832aff3cd5183d079d9ca8f2e9af82fbbfc
Matt Straathof <11823378+bruuuuuuuce@users.noreply.github.com>
2021-11-04 16:35:59 -0700

    fix: momento-cli homepage (#6)

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -1,6 +1,6 @@
 class MomentoCli < Formula
   desc "Cli to interact with Momento services"
-  homepage ""https://github.com/momentohq/momento-cli
+  homepage "https://github.com/momentohq/momento-cli"
   url "https://github.com/momentohq/momento-cli/releases/download/v0.1.1/archive-0.1.1.tar.gz"
   sha256 "19cc8336d0d0eb2e321ccf0eb3421e0e2854237dd64febefe23108f962b6f401"
   license ""
