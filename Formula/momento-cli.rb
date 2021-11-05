742143bfd86f17aa1d362d24dfb7c62f2be8e38d
Matt Straathof <matt.straat@gmail.com>
2021-11-04 21:35:48 -0700

    fix: style momento-cli formula

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -2,15 +2,14 @@ class MomentoCli < Formula
   desc "Cli to interact with Momento services"
   homepage "https://github.com/momentohq/momento-cli"
   url "https://github.com/momentohq/momento-cli/releases/download/v0.1.1/archive-0.1.1.tar.gz"
-  sha256 "19cc8336d0d0eb2e321ccf0eb3421e0e2854237dd64febefe23108f962b6f401"
   version "0.1.1"
+  sha256 "19cc8336d0d0eb2e321ccf0eb3421e0e2854237dd64febefe23108f962b6f401"
   head "https://github.com/momentohq/momento-cli.git"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build", "--release"
+    system "cargo", "install", *std_cargo_args
     bin.install "target/release/momento"
   end
-
 end
