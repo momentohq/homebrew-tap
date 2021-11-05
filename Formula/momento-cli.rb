4ac6ec37340c00d3ab26d539e3e89ba49422118a
Matt Straathof <11823378+bruuuuuuuce@users.noreply.github.com>
2021-11-04 21:27:17 -0700

    fix: momento-cli homebrew formula

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -3,11 +3,14 @@ class MomentoCli < Formula
   homepage "https://github.com/momentohq/momento-cli"
   url "https://github.com/momentohq/momento-cli/releases/download/v0.1.1/archive-0.1.1.tar.gz"
   sha256 "19cc8336d0d0eb2e321ccf0eb3421e0e2854237dd64febefe23108f962b6f401"
-  license ""
+  version "0.1.1"
+  head "https://github.com/momentohq/momento-cli.git"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "build", "--release"
+    bin.install "target/release/momento"
   end
+
 end
