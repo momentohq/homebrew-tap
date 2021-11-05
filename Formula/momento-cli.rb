66df669324dfc59800ed3c6a4ee28093682f23d2
Matt Straathof <11823378+bruuuuuuuce@users.noreply.github.com>
2021-11-05 02:56:36 -0700

    Update momento-cli.rb

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -2,7 +2,6 @@ class MomentoCli < Formula
   desc "Momento Client CLI"
   homepage "https://github.com/momentohq/momento-cli"
   url "https://github.com/momentohq/momento-cli/releases/download/v0.1.5/archive-0.1.5.tar.gz"
-  version "0.1.5"
   sha256 "67487ffffff275e4c316a38e2248d7eea70f9f9e2d79d0df8a6abe14a933f46b"
   head "https://github.com/momentohq/momento-cli.git"
 
