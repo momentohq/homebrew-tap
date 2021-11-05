7c1c8d31514d412ec602fcefc0249a6c1155cbda
Matt Straathof <matt.straat@gmail.com>
2021-11-05 03:33:00 -0700

    momento-cli 0.1.5

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -1,8 +1,8 @@
 class MomentoCli < Formula
   desc "Cli to interact with Momento services"
   homepage "https://github.com/momentohq/momento-cli"
-  url "https://github.com/momentohq/momento-cli/releases/download/v0.1.1/archive-0.1.1.tar.gz"
-  sha256 "47252d586ad50a4fd40cc10d8fd8bdc7768a84d41b9e5465d256d021a70d5371"
+  url "https://github.com/momentohq/momento-cli/releases/download/v0.1.5/archive-0.1.5.tar.gz"
+  sha256 "67487ffffff275e4c316a38e2248d7eea70f9f9e2d79d0df8a6abe14a933f46b"
   head "https://github.com/momentohq/momento-cli.git"
 
   depends_on "rust" => :build
