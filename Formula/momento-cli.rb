79b18edc681f1a5b9db613643d0d7680a9e5f4f1
Matt Straathof <11823378+bruuuuuuuce@users.noreply.github.com>
2021-11-05 01:55:28 -0700

    fix: momento-cli formula sha

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -3,7 +3,7 @@ class MomentoCli < Formula
   homepage "https://github.com/momentohq/momento-cli"
   url "https://github.com/momentohq/momento-cli/releases/download/v0.1.1/archive-0.1.1.tar.gz"
   version "0.1.1"
-  sha256 "8c3073cf893dcd14090c11e063019dc988fcbfa6b0a858664f603dd6c1d9bddf"
+  sha256 "be407f449a19511042870ae803f2573ab923285af8a071edeacf06deddf802a8"
   head "https://github.com/momentohq/momento-cli.git"
 
   depends_on "rust" => :build
