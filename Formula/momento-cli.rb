c7823d8ca059679f1f9f55950f3beb4239214073
Matt Straathof <11823378+bruuuuuuuce@users.noreply.github.com>
2021-11-05 11:46:11 +0000

    momento-cli: add 0.1.5 bottle.

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -5,6 +5,12 @@ class MomentoCli < Formula
   sha256 "67487ffffff275e4c316a38e2248d7eea70f9f9e2d79d0df8a6abe14a933f46b"
   head "https://github.com/momentohq/momento-cli.git"
 
+  bottle do
+    root_url "https://github.com/momentohq/homebrew-tap/releases/download/momento-cli-0.1.5"
+    sha256 cellar: :any_skip_relocation, catalina:     "5ed43f6a378d89ae71303a2d2b3b8900601cc4ffd99af86944a969106f14e971"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "62d393edbce042f332bd7dbb6de80a6cd2c24ae389a2952380f7b2ce9dc498c6"
+  end
+
   depends_on "rust" => :build
 
   def install
