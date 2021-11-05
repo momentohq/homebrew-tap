f31456a2f1a826af352479be53e8f181f9fee5b4
Matt Straathof <11823378+bruuuuuuuce@users.noreply.github.com>
2021-11-05 02:56:21 -0700

    chore: remove version from momento-cli formula

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -2,7 +2,6 @@ class MomentoCli < Formula
   desc "Cli to interact with Momento services"
   homepage "https://github.com/momentohq/momento-cli"
   url "https://github.com/momentohq/momento-cli/releases/download/v0.1.1/archive-0.1.1.tar.gz"
-  version "0.1.1"
   sha256 "47252d586ad50a4fd40cc10d8fd8bdc7768a84d41b9e5465d256d021a70d5371"
   head "https://github.com/momentohq/momento-cli.git"
 
