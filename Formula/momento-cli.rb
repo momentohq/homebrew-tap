1874172b06729b18094011ff1d0ed0eb1ab1b186
Matt Straathof <matt.straat@gmail.com>
2021-11-04 18:17:43 -0700

    feat: bump cli version to 0.1.2

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -1,8 +1,8 @@
 class MomentoCli < Formula
-  desc "Cli to interact with Momento services"
+  desc "Momento Client CLI"
   homepage "https://github.com/momentohq/momento-cli"
-  url "https://github.com/momentohq/momento-cli/releases/download/v0.1.1/archive-0.1.1.tar.gz"
-  sha256 "19cc8336d0d0eb2e321ccf0eb3421e0e2854237dd64febefe23108f962b6f401"
+  url "https://github.com/momentohq/momento-cli/releases/download/v0.1.2/archive-0.1.2.tar.gz"
+  sha256 "8c3073cf893dcd14090c11e063019dc988fcbfa6b0a858664f603dd6c1d9bddf"
   license ""
 
   depends_on "rust" => :build
