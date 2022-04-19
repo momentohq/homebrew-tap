e73bc9953a4ff7e635c8a199be2178a4fecf80f1
Matt Straathof <11823378+bruuuuuuuce@users.noreply.github.com>
2022-04-19 17:47:01 +0000

    momento-cli: update 0.13.26 bottle.

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -6,9 +6,9 @@ class MomentoCli < Formula
   head "https://github.com/momentohq/momento-cli.git"
 
   bottle do
-    root_url "https://github.com/momentohq/homebrew-tap/releases/download/momento-cli-0.13.25"
-    sha256 cellar: :any_skip_relocation, big_sur:      "9e66a83fef8df49b43e9dd15ac690d36c30c456d293e4d785bf7b732dfd773c4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "58039bfecebced8295eefa2f6e2fee2f4109582ce1b1147a671a59e83a22b93d"
+    root_url "https://github.com/momentohq/homebrew-tap/releases/download/momento-cli-0.13.26"
+    sha256 cellar: :any_skip_relocation, big_sur:      "e7035f5570b4a1194b3840027dc83ee0bc329e3491c526329f477f13fc402b7e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "798f7c5c4095a2420af083d68ed0dcdfae1f89fa9532e5f9d6448b0b8c7f570f"
   end
 
   depends_on "rust" => :build
