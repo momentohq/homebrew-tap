2a0f8c6b8a7b7105ac5b1f748c33b85edd304934
momentobot <momentobot@users.noreply.github.com>
2022-04-20 01:21:42 +0000

    momento-cli 0.13.33

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -1,8 +1,8 @@
 class MomentoCli < Formula
   desc "Cli to interact with Momento services"
   homepage "https://github.com/momentohq/momento-cli"
-  url "https://github.com/momentohq/momento-cli/releases/download/v0.13.32/archive-0.13.32.tar.gz"
-  sha256 "46d28d05a9de0fe892b649fe4bfd547ef77bd4cbefeb52a2c20fd02db5c0f5ff"
+  url "https://github.com/momentohq/momento-cli/releases/download/v0.13.33/archive-0.13.33.tar.gz"
+  sha256 "fa09110a0ca1434f5ccb1ed86c52fbc56d0f8a05564c94587dcd87771e9b0c7b"
   head "https://github.com/momentohq/momento-cli.git"
 
   bottle do
