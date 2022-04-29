dfaa0f253c46eee9aca618b4d04c9b62eb71893d
momentobot <momentobot@users.noreply.github.com>
2022-04-29 17:28:34 +0000

    momento-cli 0.16.0
    Closes #95.
    
    Signed-off-by: momento-github-actions-machine-user <87725926+momento-github-actions-machine-user@users.noreply.github.com>

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -1,8 +1,8 @@
 class MomentoCli < Formula
   desc "Cli to interact with Momento services"
   homepage "https://github.com/momentohq/momento-cli"
-  url "https://github.com/momentohq/momento-cli/releases/download/v0.15.0/archive-0.15.0.tar.gz"
-  sha256 "eb5a5972c272810c2b177a4d095eaa10f35051236dca3ed03c3833411f97e83b"
+  url "https://github.com/momentohq/momento-cli/releases/download/v0.16.0/archive-0.16.0.tar.gz"
+  sha256 "9ec40f9d2effed0e4f7173d5b913edef96f5ffd6c0430f22ce7604eb2714aaa0"
   head "https://github.com/momentohq/momento-cli.git"
 
   bottle do
