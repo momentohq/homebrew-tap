2c2f23e4b15a9f8b57687f68ecfb99afb27876cd
Anita Ruangrotsakun <138700973+anitarua@users.noreply.github.com>
2025-10-08 10:08:41 -0700

    fix: update template to avoid sorbet syntax errors (#201)
    * fix: update template to avoid sorbet syntax errors
    
    * fix: update non-template file too

--- Formula/momento-cli.rb
+++ Formula/momento-cli.rb
@@ -8,7 +8,7 @@ class MomentoCli < Formula
       url "https://github.com/momentohq/momento-cli/releases/download/v0.49.3/momento-cli-0.49.3.x86_64-apple-darwin.tar.gz"
       sha256 "037db3a8578e2303ed8f478cacad99c1bbbe40bd9310892b884dcd424a92fb8e"
 
-      def install
+      define_method(:install) do
         bin.install "momento"
         bash_completion.install "bash/momento"
         zsh_completion.install "zsh/_momento"
@@ -18,7 +18,7 @@ class MomentoCli < Formula
       url "https://github.com/momentohq/momento-cli/releases/download/v0.49.3/momento-cli-0.49.3.aarch64-apple-darwin.tar.gz"
       sha256 "fbde2771f4037a0b28e468fd0c585c422199bb7bc6483e158d79df764ee17c8c"
 
-      def install
+      define_method(:install) do
         bin.install "momento"
         bash_completion.install "bash/momento"
         zsh_completion.install "zsh/_momento"
@@ -31,7 +31,7 @@ class MomentoCli < Formula
       url "https://github.com/momentohq/momento-cli/releases/download/v0.49.3/momento-cli-0.49.3.linux_x86_64.tar.gz"
       sha256 "02def6898250704db451efbc7cee222961d8edf035dcac144d3bc69b5b27fbff"
 
-      def install
+      define_method(:install) do
         bin.install "momento"
         bash_completion.install "bash/momento"
         zsh_completion.install "zsh/_momento"
@@ -41,7 +41,7 @@ class MomentoCli < Formula
       url "https://github.com/momentohq/momento-cli/releases/download/v0.49.3/momento-cli-0.49.3.linux_aarch64.tar.gz"
       sha256 "74c1b90f8d3a9e73b24c8fbc2902b800ee87fbd49913c78c4cf59799ededcaa1"
 
-      def install
+      define_method(:install) do
         bin.install "momento"
         bash_completion.install "bash/momento"
         zsh_completion.install "zsh/_momento"
